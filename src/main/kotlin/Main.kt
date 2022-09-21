284f2b27f5a41df0e92d60e33fa73847cfcdacde
Jack McGrath <jackmcgrath2k@gmail.com>
2022-09-21 19:41:00 +0100

    Changed raw strings to string templates

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>) {
 
 }
 
+kotlin.math.round
 fun printPayslip(){
 
     println ("___________________________________________________________")
@@ -35,34 +36,6 @@ fun printPayslip(){
 
 }
 
-/*fun printPayslipsWithRounding(){
-    println ("___________________________________________________________")
-    println ("|   Monthly Payslip                                       |")
-    println ("|_________________________________________________________|")
-    println ("|                                                         |")
-    println ("|   Employee Name:  " + firstName.uppercase() + " " + surname.uppercase() + "(" + gender.uppercase() + ")   Employee ID: " + employeeID + "    |")
-    println ("|                                                         |")
-    println ("|_________________________________________________________|")
-    println ("|                                                         |")
-    println ("|   Payment Details \t\t\t\t Deduction Detail  \t\t\t\t  |")
-    println ("|_________________________________________________________|")
-    println ("|   Salary: " + "%.2f".format(monthlySalary) + "\t\t PAYE: " + "%.2f".format(monthlyPaye) + "\t\t\t|")
-    println ("|   Bonus:  " + "%.2f".format((annualBonus / 12)) + "\t\t\t\t PRSI: " + "%.2f".format(monthlyPrsi)  + "\t\t\t|")
-    println ("|    \t\t\t\t\t\t\t\t Cycle To Work: " + cycleToWorkMonthlyDeduction  + "\t\t\t\t|")
-    println ("|_________________________________________________________|")
-    println ("|   Gross:  " + "%.2f".format(grossPay) + " \t\tTotal Deductions: " + "%.2f".format(totalDeductions) + " |")
-    println ("|_________________________________________________________|")
-    println ("|   \t\t\t\t Net Pay:" + "%.2f".format((grossPay - totalDeductions)) + " \t\t\t\t\t\t\t|")
-    println ("|_________________________________________________________|")
-
-    val monthlySalary = (grossSalary/12)
-    val monthlyPrsi = monthlySalary * (prsiPercentage / 100)
-    val monthlyPaye = monthlySalary * (payePercentage / 100)
-    val grossPay = (monthlySalary + (annualBonus/12))
-    val totalDeductions = (monthlyPrsi + monthlyPrsi + cycleToWorkMonthlyDeduction)
-
-}
-*/
 
 val firstName = "Joe"
 val surname = "Soap"
