36405d9c5e94a5725f01ad51ed96ac7ab7411038
Jack McGrath <jackmcgrath2k@gmail.com>
2022-09-22 22:11:29 +0100

    Excercise 9 complete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,15 +13,40 @@ val annualBonus = 1450.50
 val cycleToWorkMonthlyDeduction = 54.33
 
 fun main(args: Array<String>){
-    println(getFullName())
-    println("Monthly Salary: ${getMonthlySalary()}")
-    println("Monthly PRSI: ${getMonthlyPRSI()}")
-    println("Monthly PAYE: ${getMonthlyPAYE()}")
-    println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
-    println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
-    println("Monthly Net Pay: ${getNetMonthlyPay()}")
-    printPayslip()
+
+    var input : Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 ->println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
 }
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readLine()!!.toInt()
+}
+
 fun getFullName() = when (gender){
 
     'm', 'M' -> "Mr. $firstName $surname"
@@ -38,7 +63,7 @@ fun getGrossMonthlyPay() = roundTwoDecimals(getMonthlySalary() + (annualBonus / 
 fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
 fun getNetMonthlyPay() = roundTwoDecimals(roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions()))
 
-fun printPayslip() {
+fun getPayslip() {
 
     val monthlySalary = roundTwoDecimals(grossSalary / 12)
     val monthlyPrsi = roundTwoDecimals(monthlySalary * (prsiPercentage / 100))
@@ -51,23 +76,22 @@ fun printPayslip() {
         ______________________________________________________________________
          Monthly Payslip:             ${firstName.uppercase()} ${surname.uppercase()} (${gender.uppercase()}), ID: $employeeID                  
         ______________________________________________________________________    
-              PAYMENT DETAILS (gross pay: $grossPay)                                                                    
+         Pay Details (gross pay: $grossPay)                                                                    
         ______________________________________________________________________
-                   Salary: $monthlySalary
-                   Bonus:  ${roundTwoDecimals(annualBonus / 12)}            
+         Salary: $monthlySalary
+         Bonus:  ${roundTwoDecimals(annualBonus / 12)}            
         ______________________________________________________________________
-              DEDUCTION DETAILS (total Deductions: $totalDeductions)      
+         Deduction Details (total Deductions: $totalDeductions)      
         ______________________________________________________________________
-                   PAYE: $monthlyPaye                
-                   PRSI: $monthlyPrsi  
-                   Cycle To Work: $cycleToWorkMonthlyDeduction         
+         PAYE: $monthlyPaye                
+         PRSI: $monthlyPrsi  
+         Cycle To Work: $cycleToWorkMonthlyDeduction         
         ______________________________________________________________________
-             NET PAY: ${roundTwoDecimals(grossPay - totalDeductions)} 
+         Net pay: ${roundTwoDecimals(grossPay - totalDeductions)} 
         ______________________________________________________________________"""
     )
 }
-//https://discuss.kotlinlang.org/t/how-do-you-round-a-number-to-n-decimal-places/8843
-//There are several options...try each of them out
+
 fun roundTwoDecimals(number: Double) = round(number * 100) / 100
 //fun roundTwoDecimals(number: Double) = "%.2f".format(number).toDouble()
 
